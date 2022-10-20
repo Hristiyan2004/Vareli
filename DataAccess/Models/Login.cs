5d8071b49cddfbdb6d3dce14ba5a3370374304bb
STUDENTS-STEM-9 <STUDENTS-STEM-9@DESKTOP-95SOH88>
2022-10-20 09:37:02 +0300

    Login

---
+++ DataAccess/Models/Login.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Models
+{
+    public class Login
+    {
+        public int LoginId { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public Guid UserId { get; set; }
+    }
+}
