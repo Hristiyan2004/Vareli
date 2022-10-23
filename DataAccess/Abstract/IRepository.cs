7c2e8d08de9e55d4aa64dfbf5336e151a202d01a
Hristiyan2004 <107183933+Hristiyan2004@users.noreply.github.com>
2022-10-23 18:47:16 +0300

    add Irepository (#7)
    Co-authored-by: STUDENTS-STEM-10 <STUDENTS-STEM-10@DESKTOP-9UCL5HL>

---
+++ DataAccess/Abstract/IRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Abstract
+{
+        public interface IRepository<T> where T: class
+        {
+            T GetById(dynamic id);
+            IEnumerable<T> List();
+            void Add(T entity);
+            void Delete(T entity);
+            void Edit(T entity);
+
+
+        }
+}
