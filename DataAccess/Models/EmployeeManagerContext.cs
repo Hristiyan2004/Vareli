b2b3f2659c894ba1cf962f18ad9994bd917524c5
Zhivko Madzhev <z.madzhev@consultcommerce.com>
2022-11-03 07:39:09 +0000

    add role repository

---
+++ DataAccess/Models/EmployeeManagerContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Models
+{
+	public class EmployeeManagerContext : DbContext
+	{
+		public DbSet<Role> Roles { get; set; }
+		public DbSet<Position> Positions { get; set; }
+
+
+
+		protected override void OnConfiguring(DbContextOptionsBuilder builder)
+		{
+			builder.UseSqlServer(@"");
+		}
+	}
+}
--- EmployeeManagerConsole/Program.cs
+++ EmployeeManagerConsole/Program.cs
@@ -1,10 +1,33 @@
-﻿namespace EmployeeManagerConsole
+﻿using DataAccess.Implementation;
+using DataAccess.Models;
+using System.Security.Principal;
+
+namespace EmployeeManagerConsole
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+
+            int repoId = int.Parse(Console.ReadLine());
+
+
+            var roleRepository = new RoleRepository();
+
+            int actionId = int.Parse(Console.ReadLine());
+
+
+            if(actionId == 4)
+            {
+                //todo: enter Role properties fromc console
+
+                Role payload = new Role();
+
+
+                roleRepository.Add(payload);
+
+            }
         }
     }
 }
