26c2e3f1e19ff67978c18c64d74c13cc315cb0d3
Jivko <113294149+jivko-m@users.noreply.github.com>
2022-11-03 09:40:51 +0200

    add role repository (#11)
    Co-authored-by: Zhivko Madzhev <z.madzhev@consultcommerce.com>

---
+++ DataAccess/Implementation/RoleRepository.cs
@@ -0,0 +1,46 @@
+﻿using DataAccess.Abstract;
+using DataAccess.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Implementation
+{
+	public class RoleRepository : IRepository<Role>
+	{
+		private readonly EmployeeManagerContext _context;
+		
+		public RoleRepository()
+		{
+			this._context = new EmployeeManagerContext();
+		}
+
+		public void Add(Role entity)
+		{
+			_context.Roles.Add(entity);
+			_context.SaveChanges();
+		}
+
+		public void Delete(Role entity)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void Edit(Role entity)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Role GetById(dynamic id)
+		{
+			throw new NotImplementedException();
+		}
+
+		public IEnumerable<Role> List()
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
--- DataAccess/Models/User.cs
+++ DataAccess/Models/User.cs
@@ -1,15 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace DataAccess.Models
 {
-    public class User
-    {
-        public int RoleId { get;set; }
-        public string RoleName { get;set; } 
-        public ICollection<User> Users { get; set; }
-    }
+	public class User
+	{
+		[DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+		public Guid UserId { get; set; }
+
+		public int RoleId { get; set; }
+
+
+		public virtual Role Role { get; set; }
+
+	}
 }
